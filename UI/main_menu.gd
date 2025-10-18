bc5ff5658aa7bc92e549c1ea1c60dca736373dd6
Satanas1275 <107134873+Satanas1275@users.noreply.github.com>
2025-10-18 18:41:48 +0200

    Add MainMenu

---
+++ UI/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_Button_Jouer_pressed():
+	# Charge ta scène de jeu
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_Button_Options_pressed():
+	# Exemple : afficher une popup ou une autre scène d’options
+	print("Ouvrir le menu des options")
+
+func _on_Button_Quitter_pressed():
+	# Ferme le jeu
+	get_tree().quit()
