36dd8d1476d578a0953a6754a8356d421feb6e20
Satanas1275 <107134873+Satanas1275@users.noreply.github.com>
2025-10-25 12:28:15 +0200

    update - pages
    - ajout des script pour les pages
    - ajout des pages dans la map
    - ajout des son quand on trouve une page
    - j'ais pas dutout galérer...

---
+++ script/page6.gd
@@ -0,0 +1,34 @@
+extends Area3D
+
+@export var page_index: int = 6  # Numéro de la page (0 à 8)
+@onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
+
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+func _on_body_entered(body):
+	if not body.is_in_group("player"):
+		return
+
+	if page_index in Global.pages_trouvees:
+		return
+
+	Global.pages_trouvees.append(page_index)
+	print("📘 Page trouvée :", page_index, "| Total :", Global.pages_trouvees.size(), "/ 9")
+
+	play_random_sound()
+
+	# Supprime la page après le son
+	await get_tree().create_timer(1).timeout
+	queue_free()
+
+func play_random_sound():
+	var random_id = randi_range(1, 4)
+	var sound_path = "res://assets/audio/doublage/Trouvé!" + ("" if random_id == 1 else str(random_id)) + ".wav"
+
+	var sound = load(sound_path)
+	if sound:
+		audio_player.stream = sound
+		audio_player.play()
+	else:
+		print("⚠️ Fichier introuvable :", sound_path)
